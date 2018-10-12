d4a8c521a07ad2a48275c91a62bd61d00086031f
Robinlp <Robinlp@users.noreply.github.com>
2018-10-12 11:01:10 +0200

    test

--- testGithub/src/testgithub/TestGithub.java
+++ testGithub/src/testgithub/TestGithub.java
@@ -12,6 +12,8 @@ public class TestGithub {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("det virker");
+        
+        System.out.println("pøls");
     }
     
 }
